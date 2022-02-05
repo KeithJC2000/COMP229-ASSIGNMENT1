d90005cd08946ce8b645a11ede319df1a5cb15d5
KeithJC2000 <jiangchen001012@gmail.com>
2022-02-05 17:12:55 -0500

    AS1

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-
-/* GET users listing. */
-router.get('/chen', function(req, res, next) {
-  res.send('Welcome Chen');
+  res.send('Placeholder');
 });
 
 module.exports = router;
+
